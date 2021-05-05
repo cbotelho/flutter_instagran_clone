419ad5e284c5e20fa7b892a3eb6588e4bc9f5f5f
qndincl <39212474+qndincl@users.noreply.github.com>
2021-05-05 12:14:23 +0900

    Fix feed_screen, main

--- lib/feed_screen.dart
+++ lib/feed_screen.dart
@@ -1,15 +1,49 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_codingpapa/widgets/post.dart';
 
 class FeedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
+      appBar: CupertinoNavigationBar(
+        leading: IconButton(
+          onPressed: null,
+          icon: Icon(
+            Icons.camera_alt,
+            color: Colors.black87,
+          ),
+        ),
+        middle: Text(
           'instagram',
-          style: TextStyle(fontFamily: 'VaganStyle'),
+          style: TextStyle(fontFamily: 'VeganStyle'),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            IconButton(
+                icon: ImageIcon(
+                  AssetImage('assets/images/actionbar_camera.png'),
+                  color: Colors.black87,
+                ),
+                onPressed: null),
+            IconButton(
+                icon: ImageIcon(
+                  AssetImage('assets/images/actionbar_camera.png'),
+                  color: Colors.black87,
+                ),
+                onPressed: null)
+          ],
         ),
       ),
+      body: ListView.builder(
+        itemBuilder: feedListBuilder,
+        itemCount: 30,
+      ),
     );
+  }
+
+  Widget feedListBuilder(BuildContext context, int index) {
+    return Post(index);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_codingpapa/constants/material_white.dart';
 import 'package:flutter_instagram_codingpapa/home_page.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      theme: ThemeData(primarySwatch: white),
     );
   }
 }
