89b0266746020e90e75c0b2576e2a6454192e1b4
qndincl <39212474+qndincl@users.noreply.github.com>
2021-05-05 14:11:03 +0900

    Make selectedIndicator & Using Enum(_selectedTab) in ProfileBody

--- lib/widgets/profile_body.dart
+++ lib/widgets/profile_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_codingpapa/constants/common_size.dart';
+import 'package:flutter_instagram_codingpapa/constants/screen_size.dart';
 
 class ProfileBody extends StatefulWidget {
   @override
@@ -7,7 +8,8 @@ class ProfileBody extends StatefulWidget {
 }
 
 class _ProfileBodyState extends State<ProfileBody> {
-  bool selectedLeft = true;
+  // bool selectedLeft = true;
+  SelectedTab _selectedTab = SelectedTab.left;
 
   @override
   Widget build(BuildContext context) {
@@ -21,43 +23,67 @@ class _ProfileBodyState extends State<ProfileBody> {
               _username(),
               _usernBio(),
               _editProfileBtn(), //? CustomScrollView 안에 SliverList로 넣으니까 가로를 꽉 채우는 버튼이 되었다.
-              Row(
-                // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                // spaceAround로 하면 딱 이미지 크기만 터치가 가능하지만 아래처럼
-                // Expanded로 감싸면 넓은 범위를 터치할수있게되니 이게 더 좋다.
-                children: <Widget>[
-                  Expanded(
-                    child: IconButton(
-                      icon: ImageIcon(
-                        AssetImage('assets/images/grid.png'),
-                        color: selectedLeft ? Colors.black : Colors.black26,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          selectedLeft = true;
-                        });
-                      },
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                      icon: ImageIcon(
-                        AssetImage('assets/images/saved.png'),
-                        color: selectedLeft ? Colors.black26 : Colors.black,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          selectedLeft = false;
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              ),
+              _tabButtons(),
+              _selectedIndicator(),
             ]),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _selectedIndicator() {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      alignment: _selectedTab == SelectedTab.left
+          ? Alignment.centerLeft
+          : Alignment.centerRight,
+      child: Container(
+        height: 2,
+        width: size.width / 2,
+        color: Colors.black87,
+      ),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  Row _tabButtons() {
+    return Row(
+      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+      // spaceAround로 하면 딱 이미지 크기만 터치가 가능하지만 아래처럼
+      // Expanded로 감싸면 넓은 범위를 터치할수있게되니 이게 더 좋다.
+      children: <Widget>[
+        Expanded(
+          child: IconButton(
+            icon: ImageIcon(
+              AssetImage('assets/images/grid.png'),
+              color: _selectedTab == SelectedTab.left
+                  ? Colors.black
+                  : Colors.black26,
+            ),
+            onPressed: () {
+              setState(() {
+                _selectedTab = SelectedTab.left;
+              });
+            },
+          ),
+        ),
+        Expanded(
+          child: IconButton(
+            icon: ImageIcon(
+              AssetImage('assets/images/saved.png'),
+              color: _selectedTab == SelectedTab.left
+                  ? Colors.black26
+                  : Colors.black,
+            ),
+            onPressed: () {
+              setState(() {
+                _selectedTab = SelectedTab.right;
+              });
+            },
+          ),
+        ),
+      ],
     );
   }
 
@@ -100,3 +126,5 @@ class _ProfileBodyState extends State<ProfileBody> {
     );
   }
 }
+
+enum SelectedTab { left, right }
