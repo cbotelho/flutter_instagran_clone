5da14f91845e00fbb6e13ace0b5a7405b3511388
qndincl <39212474+qndincl@users.noreply.github.com>
2021-05-05 13:00:08 +0900

    Make _username()

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_codingpapa/constants/common_size.dart';
 
 class ProfileScreen extends StatelessWidget {
   @override
@@ -7,8 +8,22 @@ class ProfileScreen extends StatelessWidget {
       backgroundColor: Colors.grey[100],
       body: SafeArea(
         child: Column(
-          children: <Widget>[_appbar()],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            _appbar(),
+            _username(),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _username() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: common_gap),
+      child: Text(
+        "username",
+        style: TextStyle(fontWeight: FontWeight.bold),
       ),
     );
   }
